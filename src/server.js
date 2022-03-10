cd833f69b6bab6b1cc2b8f30d22fcdd4fa868ade
asrofil <riidloa@gmail.com>
2022-03-10 09:18:48 +0700

    update

--- src/server.js
+++ src/server.js
@@ -9,18 +9,22 @@ const InternalServerError = require('./exceptions/InternalServerError');
 // plugins
 const songs = require('./api/songs');
 const albums = require('./api/albums');
+const users = require('./api/users');
 
 // services
 const SongsService = require('./services/postgres/SongsServices');
 const AlbumsService = require('./services/postgres/AlbumsServices');
+const UsersService = require('./services/postgres/UsersServices');
 
 // validators
 const SongValidator = require('./validator/songs');
 const AlbumValidator = require('./validator/albums');
+const UserValidator = require('./validator/users');
 
 const init = async () => {
   const songsService = new SongsService();
   const albumsService = new AlbumsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -45,28 +49,34 @@ const init = async () => {
       service: albumsService,
       validator: AlbumValidator,
     },
+  },
+  {
+    plugin: users,
+    options: {
+      service: usersService,
+      validator: UserValidator,
+    },
   }]);
 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
     if (response instanceof ClientError) {
-        // membuat response baru dari response toolkit sesuai kebutuhan error handling
-        const newResponse = h.response({
-            status: 'fail',
-            message: response.message,
-        });
-        newResponse.code(response.statusCode);
-        return newResponse;
-    } else if (response instanceof InternalServerError) {
-        const newResponse = h.response({
+      // membuat response baru dari response toolkit sesuai kebutuhan error handling
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    } if (response instanceof InternalServerError) {
+      const newResponse = h.response({
         status: 'error',
         message: response.message,
       });
       newResponse.code(response.statusCode);
       return newResponse;
     }
-
     // jika bukan ClientError, lanjutkan dengan response sebelumnya (tanpa terintervensi)
     return response.continue || response;
   });
