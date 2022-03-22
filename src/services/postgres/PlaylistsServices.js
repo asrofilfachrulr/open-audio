76d0773ed9e066991b192cab104c111fc613673e
asrofil <riidloa@gmail.com>
2022-03-22 21:56:20 +0700

    finish query get /playlists for collab

--- src/services/postgres/PlaylistsServices.js
+++ src/services/postgres/PlaylistsServices.js
@@ -28,7 +28,7 @@ class PlaylistsServices {
 
   async getPlaylists(ownerId) {
     const query = {
-      text: 'SELECT playlists.id, playlists.name, users.username FROM playlists LEFT JOIN users ON users.id = playlists.owner WHERE users.id = $1',
+      text: 'SELECT playlists.id, playlists.name, users.username FROM playlists LEFT JOIN collaborations ON playlists.id = collaborations.playlist_id INNER JOIN users ON users.id = playlists.owner WHERE playlists.owner = $1 OR collaborations.user_id = $1;',
       values: [ownerId],
     };
 
